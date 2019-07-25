deb42437473d1da46db498415fcee1936d20f259
stopopol <christoph.wohner@umweltbundesamt.at>
2019-07-25 16:29:48 +0200

    Add files via upload

--- src/Controller/DeimsSiteRecordController.php
+++ src/Controller/DeimsSiteRecordController.php
@@ -36,16 +36,37 @@ class DeimsSiteRecordController extends ControllerBase {
 			if ($node->bundle() == 'site' && $node->isPublished()) {
 		
 				$site_information['deimsid'] = $deimsid;
-				$site_information['name'] = $node->get('field_name')->value;
-				$site_information['short_name'] = $node->get('field_name_short')->value;
-				$site_information['site_status'] = $node->get('field_site_status')->value;
-				$site_information['coordinates'] = $node->get('field_coordinates')->value;
-				$site_information['boundaries'] = $node->get('field_boundaries')->value;
-				$site_information['purpose'] = $node->get('field_purpose')->value;
-				$site_information['biome'] = $node->get('field_biome')->value;
 				// DeimsSiteParagraphFieldController::parseAffiliation is located in a different controller file, but doesn't have to be included in order to be called
 				$site_information['affiliation'] = DeimsSiteParagraphFieldController::parseAffiliation($node);
-					
+				$site_information['air_temperature']['average'] = $node->get('field_air_temp_average')->value . ' °C';
+				$site_information['air_temperature']['min'] = $node->get('field_air_temp_min')->value . ' °C';
+				$site_information['air_temperature']['max'] = $node->get('field_air_temp_max')->value . ' °C';
+				$site_information['biogeographical_region'] = $node->get('field_biogeographical_region')->value;
+				$site_information['biome'] = $node->get('field_biome')->value;
+				$site_information['boundaries'] = $node->get('field_boundaries')->value;
+				$site_information['coordinates'] = $node->get('field_coordinates')->value;
+				$site_information['country'] = $node->get('field_country')->value;
+				$site_information['design_experiments'] = $node->get('field_design_experiments')->value;
+				$site_information['design_observation'] = $node->get('field_design_observation')->value;
+				$site_information['ecosystem_landuse'] = $node->get('field_ecosystem_land_use')->value;
+				$site_information['elevation']['average'] = $node->get('field_elevation_average')->value . ' m';
+				$site_information['elevation']['min'] = $node->get('field_elevation_min')->value . ' m';
+				$site_information['elevation']['max'] = $node->get('field_elevation_max')->value . ' m';
+				$site_information['funding_agency'] = $node->get('field_funding_agency')->value;
+				$site_information['geo_bon_biome'] = $node->get('field_geo_bon_biome')->value;
+				$site_information['geology'] = $node->get('field_geology')->value;
+				$site_information['history'] = $node->get('field_history')->value;
+				$site_information['hydrology'] = $node->get('field_hydrology')->value;				
+				$site_information['keywords'] = $node->get('field_keywords')->value;				
+				$site_information['name'] = $node->get('field_name')->value;
+				$site_information['purpose'] = $node->get('field_purpose')->value;
+				$site_information['short_name'] = $node->get('field_name_short')->value;
+				$site_information['site_status'] = $node->get('field_site_status')->value;
+				$site_information['size'] = $node->get('field_size')->value . ' ha';
+				$site_information['soils'] = $node->get('field_soils')->value;
+				$site_information['vegetation'] = $node->get('field_vegetation')->value;	
+				$site_information['year_closed'] = $node->get('field_year_closed')->value;	
+				$site_information['year_established'] = $node->get('field_year_established')->value;	
 			}
 		}
 	}
