da3d803de6f76c657830429f6e980a07509099dc
stopopol <christoph.wohner@umweltbundesamt.at>
2019-10-22 16:46:28 +0200

    Add files via upload

--- src/Controller/DeimsLandingPageController.php
+++ src/Controller/DeimsLandingPageController.php
@@ -19,14 +19,16 @@ class DeimsLandingPageController extends ControllerBase {
   public function renderLandingPage() {
     
     // https://app.swaggerhub.com/apis/klimeto/RPI2.0/0.0.1
-	  $output_information['title'] = 'DEIMS-SDR API';
-	  $output_information['description'] = 'This is an API for automated data export from the site and dataset registry DEIMS-SDR';
-    $output_information['termsOfService'] = 'https://deims.org/terms';
-    $output_information['contact']['name'] = 'DEIMS-SDR Support';
-    $output_information['contact']['url'] = 'https://deims.org/contact';
-    $output_information['license']['name'] = 'CC-BY-NC International 4.0';
-    $output_information['license']['url'] = 'https://creativecommons.org/licenses/by-nc/4.0/';
-    $output_information['version'] = '0.1';
+    $output_information['servers'] = array(array("description"=>"Official DEIMS-SDR API","url"=>"https://deims.org/api"));
+	  $output_information['info']['title'] = 'DEIMS-SDR API';
+	  $output_information['info']['description'] = 'This is an API for automated data export from the site and dataset registry DEIMS-SDR';
+    $output_information['info']['termsOfService'] = 'https://deims.org/terms';
+    $output_information['info']['contact']['name'] = 'DEIMS-SDR Support';
+    $output_information['info']['contact']['url'] = 'https://deims.org/contact';
+    $output_information['info']['license']['name'] = 'CC-BY-NC International 4.0';
+    $output_information['info']['license']['url'] = 'https://creativecommons.org/licenses/by-nc/4.0/';
+    $output_information['info']['version'] = '0.1';
+    $output_information['tags'] = array("sites","datasets","sensors","activities");
     
     return new JsonResponse($output_information);
   }
