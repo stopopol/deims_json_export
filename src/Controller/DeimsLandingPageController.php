75f6d9e656d2a13835f0e1e9032946f276324afd
stopopol <christoph.wohner@umweltbundesamt.at>
2019-10-24 14:51:26 +0200

    Add files via upload

--- src/Controller/DeimsLandingPageController.php
+++ src/Controller/DeimsLandingPageController.php
@@ -20,52 +20,27 @@ class DeimsLandingPageController extends ControllerBase {
     
     // https://app.swaggerhub.com/apis/klimeto/RPI2.0/0.0.1
     //$output_information['servers'] = array(array("description"=>"Official DEIMS-SDR API","url"=>"https://deims.org/api"));
-    $schema_folder_location = __DIR__ . '/../Schema/';
+    $definition_location = __DIR__ . '/../Custom/Path/';
     $output_information['openapi'] = '3.0.0';
-    $output_information['info']= json_decode(file_get_contents($schema_folder_location . 'info.json'));
-    $output_information['paths']['/site'] = json_decode(file_get_contents($schema_folder_location . 'site_list.json'));
-    $output_information['paths']['/site/{resource_id}'] = json_decode(file_get_contents($schema_folder_location . 'site_record.json'));
-    $output_information['paths']['/dataset'] = json_decode(file_get_contents($schema_folder_location . 'dataset_list.json'));
-    $output_information['paths']['/dataset/{resource_id}'] = json_decode(file_get_contents($schema_folder_location . 'dataset_record.json'));
-    $output_information['paths']['/activity'] = json_decode(file_get_contents($schema_folder_location . 'activity_list.json'));
-    $output_information['paths']['/activity/{resource_id}'] = json_decode(file_get_contents($schema_folder_location . 'activity_record.json'));
-    $output_information['paths']['/sensor'] = json_decode(file_get_contents($schema_folder_location . 'sensor_list.json'));
-    $output_information['paths']['/sensor/{resource_id}'] = json_decode(file_get_contents($schema_folder_location . 'sensor_record.json'));
+    $output_information['info']= json_decode(file_get_contents($definition_location . 'info.json'));
+    $output_information['paths']['/site'] = json_decode(file_get_contents($definition_location . 'site_list.json'));
+    $output_information['paths']['/site/{resource_id}'] = json_decode(file_get_contents($definition_location . 'site_record.json'));
+    $output_information['paths']['/dataset'] = json_decode(file_get_contents($definition_location . 'dataset_list.json'));
+    $output_information['paths']['/dataset/{resource_id}'] = json_decode(file_get_contents($definition_location . 'dataset_record.json'));
+    $output_information['paths']['/activity'] = json_decode(file_get_contents($definition_location . 'activity_list.json'));
+    $output_information['paths']['/activity/{resource_id}'] = json_decode(file_get_contents($definition_location . 'activity_record.json'));
+    $output_information['paths']['/sensor'] = json_decode(file_get_contents($definition_location . 'sensor_list.json'));
+    $output_information['paths']['/sensor/{resource_id}'] = json_decode(file_get_contents($definition_location . 'sensor_record.json'));
 
-  
-    // resource not found error message
-    $output_information['components']['schemas']['resourceNotFound'] = json_decode(file_get_contents(__DIR__ . '/../Schema/resource_not_found.json'));
- 
-    // components for site list
-    $output_information['components']['schemas']['siteList']['type'] = "object";
-    $output_information['components']['schemas']['siteList']['properties']['name']['type'] = "string";
-    $output_information['components']['schemas']['siteList']['properties']['deimsid']['type'] = "object";
-    $output_information['components']['schemas']['siteList']['properties']['deimsid']['properties']['prefix']['type'] = "string";
-    $output_information['components']['schemas']['siteList']['properties']['deimsid']['properties']['id']['type'] = "string";
-    $output_information['components']['schemas']['siteList']['properties']['coordinates']['type'] = "string";
-    $output_information['components']['schemas']['siteList']['properties']['changed']['type'] = "string";
+    $definition_location = __DIR__ . '/../Custom/Component/';
+    $output_information['components']['schemas']['resourceNotFound'] = json_decode(file_get_contents($definition_location . '/resource_not_found.json'));
+    $output_information['components']['schemas']['siteList'] = json_decode(file_get_contents($definition_location . '/siteList.json'));
+    $output_information['components']['schemas']['affiliationItem'] = json_decode(file_get_contents($definition_location . '/affiliationItem.json'));
+    $output_information['components']['schemas']['recordList'] = json_decode(file_get_contents($definition_location . '/recordList.json'));
 
-    $output_information['components']['schemas']['siteList']['properties']['affiliation']['type'] = "array";
-    $output_information['components']['schemas']['siteList']['properties']['affiliation']['items']['$ref'] = '#/components/schemas/affiliationItem';
-
-    $output_information['components']['schemas']['affiliationItem']['type'] = "object";
-    $output_information['components']['schemas']['affiliationItem']['properties']['network']['type'] = "string";
-    $output_information['components']['schemas']['affiliationItem']['properties']['code']['type'] = "string";
-    $output_information['components']['schemas']['affiliationItem']['properties']['verified']['type'] = "boolean";
-
-
-    // other record lists
-    $output_information['components']['schemas']['recordList']['type'] = "object";
-    $output_information['components']['schemas']['recordList']['properties']['name']['type'] = "string";
-    $output_information['components']['schemas']['recordList']['properties']['path']['type'] = "object";
-    $output_information['components']['schemas']['recordList']['properties']['path']['properties']['prefix']['type'] = "string";
-    $output_information['components']['schemas']['recordList']['properties']['path']['properties']['id']['type'] = "string";
-    $output_information['components']['schemas']['recordList']['properties']['changed']['type'] = "string";
 
     // TO DO: record components
     // Site
-    //$output_information['components']['schemas']['SiteRecord'] = json_decode(file_get_contents(__DIR__ . '/../Schema/site_record.json'));
-
     // Dataset
     // Sensor
     // Activity
