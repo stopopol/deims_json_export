ffedf118cfec1993f1ed225b897bcd1a3620db14
stopopol <christoph.wohner@umweltbundesamt.at>
2020-06-03 16:48:04 +0200

    Update DeimsActivityRecordController.php

--- src/Controller/DeimsActivityRecordController.php
+++ src/Controller/DeimsActivityRecordController.php
@@ -19,7 +19,7 @@ class DeimsActivityRecordController extends ControllerBase {
 		$DeimsFieldController = new DeimsFieldController();
 
 		$activity_information['title'] = $node->get('title')->value;
-		$activity_information['id']['prefix'] = 'https://deims.org/activity/';
+		$activity_information['id']['prefix'] = 'https://deims.org/activities/';
 		$activity_information['id']['suffix'] = $node->get('uuid')->value;
 		$activity_information['type'] = 'activity';
 		$activity_information['created'] = \Drupal::service('date.formatter')->format($node->getCreatedTime(), 'html_datetime');
