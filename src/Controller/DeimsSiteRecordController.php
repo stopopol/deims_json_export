dd35d6de035f278122229847766b8cfc4145fe31
stopopol <christoph.wohner@umweltbundesamt.at>
2022-04-21 18:25:46 +0200

    Update DeimsSiteRecordController.php

--- src/Controller/DeimsSiteRecordController.php
+++ src/Controller/DeimsSiteRecordController.php
@@ -50,7 +50,7 @@ class DeimsSiteRecordController extends ControllerBase {
 
 		// aggregate temperature fields; shorthand ifs to catch empty values
 		$site_information['attributes']['environmentalCharacteristics']['airTemperature']['avg'] = (!is_null($node->get('field_air_temp_avg')->value)) ? floatval($node->get('field_air_temp_avg')->value) : null;
-		$site_information['attributes']['environmentalCharacteristics']['airTemperature']['values'] = $DeimsFieldController->parseRegularField($node->get('field_air_temp_avg'), "number_float");
+		$site_information['attributes']['environmentalCharacteristics']['airTemperature']['values'] = $DeimsFieldController->parseRegularField($node->get('field_air_temp'), "number_float");
 		$site_information['attributes']['environmentalCharacteristics']['airTemperature']['unit'] = '°C';
 		$site_information['attributes']['environmentalCharacteristics']['airTemperature']['referencePeriod'] = $DeimsFieldController->parseEntityReferenceField($node->get('field_standard_reference_period'));
 		$site_information['attributes']['environmentalCharacteristics']['precipitation']['annual'] = (!is_null($node->get('field_precipitation_annual')->value)) ? floatval($node->get('field_precipitation_annual')->value) : null;
