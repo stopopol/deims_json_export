7f764f77f9ae9264bfa51b045f301b76872b1275
stopopol <christoph.wohner@umweltbundesamt.at>
2025-08-07 13:31:59 +0200

    Update DeimsIso19139Controller.php

--- src/Controller/DeimsIso19139Controller.php
+++ src/Controller/DeimsIso19139Controller.php
@@ -7,55 +7,31 @@ use Drupal\Core\Controller\ControllerBase;
 
 class DEIMSIso19139Controller extends ControllerBase
 {
-    public function Iso19139Response($record_information)
-    {
+    public function Iso19139Response($record_information) {
+		
         $doc = new \DOMDocument("1.0", "UTF-8");
         $doc->formatOutput = true;
 
         // Root element
-        $root = $doc->createElementNS(
-            "http://www.isotc211.org/2005/gmd",
-            "gmd:MD_Metadata"
-        );
+        $root = $doc->createElementNS("http://www.isotc211.org/2005/gmd","gmd:MD_Metadata");
         $doc->appendChild($root);
 
         // Namespaces
-        $root->setAttributeNS(
-            "http://www.w3.org/2000/xmlns/",
-            "xmlns:gco",
-            "http://www.isotc211.org/2005/gco"
-        );
-        $root->setAttributeNS(
-            "http://www.w3.org/2000/xmlns/",
-            "xmlns:xsi",
-            "http://www.w3.org/2001/XMLSchema-instance"
-        );
-        $root->setAttributeNS(
-            "http://www.w3.org/2000/xmlns/",
-            "xmlns:gml",
-            "http://www.opengis.net/gml"
-        );
-        $root->setAttribute(
-            "xsi:schemaLocation",
-            "http://www.isotc211.org/2005/gmd http://schemas.opengis.net/iso/19139/20070417/gmd/gmd.xsd"
-        );
+        $root->setAttributeNS("http://www.w3.org/2000/xmlns/","xmlns:gco","http://www.isotc211.org/2005/gco");
+        $root->setAttributeNS("http://www.w3.org/2000/xmlns/","xmlns:xsi","http://www.w3.org/2001/XMLSchema-instance");
+        $root->setAttributeNS("http://www.w3.org/2000/xmlns/","xmlns:gml","http://www.opengis.net/gml");
+        $root->setAttribute("xsi:schemaLocation","http://www.isotc211.org/2005/gmd http://schemas.opengis.net/iso/19139/20070417/gmd/gmd.xsd");
 
         // fileIdentifier
         $fileIdentifier = $doc->createElement("gmd:fileIdentifier");
-        $charStr = $doc->createElement(
-            "gco:CharacterString",
-            $record_information["id"]["suffix"] ?? "default-id"
-        );
+        $charStr = $doc->createElement("gco:CharacterString",$record_information["id"]["suffix"]);
         $fileIdentifier->appendChild($charStr);
         $root->appendChild($fileIdentifier);
 
         // language
         $language = $doc->createElement("gmd:language");
         $langCode = $doc->createElement("gmd:LanguageCode", "eng");
-        $langCode->setAttribute(
-            "codeList",
-            "http://www.loc.gov/standards/iso639-2/"
-        );
+        $langCode->setAttribute("codeList","http://www.loc.gov/standards/iso639-2/");
         $langCode->setAttribute("codeListValue", "eng");
         $language->appendChild($langCode);
         $root->appendChild($language);
@@ -63,10 +39,7 @@ class DEIMSIso19139Controller extends ControllerBase
         // characterSet
         $charSet = $doc->createElement("gmd:characterSet");
         $charCode = $doc->createElement("gmd:MD_CharacterSetCode", "utf8");
-        $charCode->setAttribute(
-            "codeList",
-            "http://www.isotc211.org/2005/resources/codeList.xml#MD_CharacterSetCode"
-        );
+        $charCode->setAttribute("codeList","http://www.isotc211.org/2005/resources/codeList.xml#MD_CharacterSetCode");
         $charCode->setAttribute("codeListValue", "utf8");
         $charSet->appendChild($charCode);
         $root->appendChild($charSet);
@@ -74,25 +47,35 @@ class DEIMSIso19139Controller extends ControllerBase
         // hierarchyLevel
         $hierarchy = $doc->createElement("gmd:hierarchyLevel");
         $scopeCode = $doc->createElement("gmd:MD_ScopeCode", "dataset");
-        $scopeCode->setAttribute(
-            "codeList",
-            "http://www.isotc211.org/2005/resources/codeList.xml#MD_ScopeCode"
-        );
+        $scopeCode->setAttribute("codeList","http://www.isotc211.org/2005/resources/codeList.xml#MD_ScopeCode");
         $scopeCode->setAttribute("codeListValue", "dataset");
         $hierarchy->appendChild($scopeCode);
         $root->appendChild($hierarchy);
+		
+		// Create <gmd:dataQualityInfo>
+		$dataQualityInfo = $doc->createElement("gmd:dataQualityInfo");
+		$dqDataQuality = $doc->createElement("gmd:DQ_DataQuality");
+
+		// <gmd:lineage>
+		$lineage = $doc->createElement("gmd:lineage");
+		$liLineage = $doc->createElement("gmd:LI_Lineage");
+		$statement = $doc->createElement("gmd:statement");
+		$statementText = "This metadata records was created using informaton from DEIMS-SDR (deims.org) and is based on user input via forms.";
+		$charString = $doc->createElement("gco:CharacterString");
+		$charString->appendChild($doc->createTextNode($statementText));
+		$statement->appendChild($charString);
+		$liLineage->appendChild($statement);
+		$lineage->appendChild($liLineage);
+		$dqDataQuality->appendChild($lineage);
+		$dataQualityInfo->appendChild($dqDataQuality);
+		$root->appendChild($dataQualityInfo);
 
         // contact
         $contact = $doc->createElement("gmd:contact");
         $root->appendChild($contact);
 
-        if (
-            !empty($record_information["attributes"]["contact"]["siteManager"])
-        ) {
-            foreach (
-                $record_information["attributes"]["contact"]["siteManager"]
-                as $person
-            ) {
+        if (!empty($record_information["attributes"]["contact"]["siteManager"])) {
+            foreach ($record_information["attributes"]["contact"]["siteManager"] as $person) {
                 $rp = $doc->createElement("gmd:CI_ResponsibleParty");
                 $name = $doc->createElement("gmd:individualName");
                 $nameText = $doc->createTextNode($person["name"] ?? "");
@@ -117,19 +100,8 @@ class DEIMSIso19139Controller extends ControllerBase
             }
         }
 
-        if (
-            !empty(
-                $record_information["attributes"]["contact"][
-                    "operatingOrganisation"
-                ]
-            )
-        ) {
-            foreach (
-                $record_information["attributes"]["contact"][
-                    "operatingOrganisation"
-                ]
-                as $organisation
-            ) {
+        if (!empty($record_information["attributes"]["contact"]["operatingOrganisation"])) {
+            foreach ($record_information["attributes"]["contact"]["operatingOrganisation"] as $organisation) {
                 $rp = $doc->createElement("gmd:CI_ResponsibleParty");
                 $name = $doc->createElement("gmd:organisationName");
                 $nameText = $doc->createTextNode($organisation["name"] ?? "");
@@ -138,14 +110,8 @@ class DEIMSIso19139Controller extends ControllerBase
 
                 // role is always mandatory for CI_ResponsibleParty
                 $role = $doc->createElement("gmd:role");
-                $roleCode = $doc->createElement(
-                    "gmd:CI_RoleCode",
-                    "pointOfContact"
-                );
-                $roleCode->setAttribute(
-                    "codeList",
-                    "http://www.isotc211.org/2005/resources/codeList.xml#CI_RoleCode"
-                );
+                $roleCode = $doc->createElement("gmd:CI_RoleCode", "pointOfContact");
+                $roleCode->setAttribute("codeList","http://www.isotc211.org/2005/resources/codeList.xml#CI_RoleCode");
                 $roleCode->setAttribute("codeListValue", "pointOfContact");
                 $role->appendChild($roleCode);
                 $rp->appendChild($role);
@@ -160,15 +126,11 @@ class DEIMSIso19139Controller extends ControllerBase
 
         // metadataStandard
         $stdName = $doc->createElement("gmd:metadataStandardName");
-        $stdName->appendChild(
-            $doc->createElement("gco:CharacterString", "ISO 19115:2003/19139")
-        );
+        $stdName->appendChild($doc->createElement("gco:CharacterString", "ISO 19115:2003/19139"));
         $root->appendChild($stdName);
 
         $stdVersion = $doc->createElement("gmd:metadataStandardVersion");
-        $stdVersion->appendChild(
-            $doc->createElement("gco:CharacterString", "1.0")
-        );
+        $stdVersion->appendChild($doc->createElement("gco:CharacterString", "1.0"));
         $root->appendChild($stdVersion);
 
         // identificationInfo
@@ -179,37 +141,24 @@ class DEIMSIso19139Controller extends ControllerBase
         $citation = $doc->createElement("gmd:citation");
         $ciCitation = $doc->createElement("gmd:CI_Citation");
         $title = $doc->createElement("gmd:title");
-        $title->appendChild(
-            $doc->createElement(
-                "gco:CharacterString",
-                $record_information["title"]
-            )
-        );
+        $title->appendChild($doc->createElement("gco:CharacterString",$record_information["title"]));
         $ciCitation->appendChild($title);
 
         // citation > date
         $ciDate = $doc->createElement("gmd:CI_Date");
 
         $date = $doc->createElement("gmd:date");
-        $dateVal = $doc->createElement(
-            "gco:Date",
-            $record_information["changed"]
-        );
+        $dateVal = $doc->createElement("gco:Date",$record_information["changed"]);
         $date->appendChild($dateVal);
         $ciDate->appendChild($date);
 
         $dateType = $doc->createElement("gmd:dateType");
         $dtCode = $doc->createElement("gmd:CI_DateTypeCode", "creation");
-        $dtCode->setAttribute(
-            "codeList",
-            "http://www.isotc211.org/2005/resources/codeList.xml#CI_DateTypeCode"
-        );
+        $dtCode->setAttribute("codeList","http://www.isotc211.org/2005/resources/codeList.xml#CI_DateTypeCode");
         $dtCode->setAttribute("codeListValue", "creation");
         $dateType->appendChild($dtCode);
         $ciDate->appendChild($dateType);
-        $ciCitation
-            ->appendChild($doc->createElement("gmd:date"))
-            ->appendChild($ciDate);
+        $ciCitation->appendChild($doc->createElement("gmd:date"))->appendChild($ciDate);
         $citation->appendChild($ciCitation);
         $dataId->appendChild($citation);
 
@@ -222,12 +171,7 @@ class DEIMSIso19139Controller extends ControllerBase
         // abstract
         $abstract = $doc->createElement("gmd:abstract");
         $abstractText = $doc->createElement("gco:CharacterString");
-        $abstractText->appendChild(
-            $doc->createTextNode(
-                $record_information["attributes"]["general"]["abstract"] ??
-                    "No abstract provided."
-            )
-        );
+        $abstractText->appendChild($doc->createTextNode($record_information["attributes"]["general"]["abstract"] ?? "No abstract provided."));
         $abstract->appendChild($abstractText);
         $dataId->appendChild($abstract);
 
@@ -237,20 +181,14 @@ class DEIMSIso19139Controller extends ControllerBase
 
         // useLimitation with license URL and name
         $useLimitation = $doc->createElement("gmd:useLimitation");
-        $licenseText =
-            "This dataset is licenced under the Creative Commons Attribution 4.0 International (CC BY 4.0). See https://creativecommons.org/licenses/by/4.0/";
-        $useLimitation->appendChild(
-            $doc->createElement("gco:CharacterString", $licenseText)
-        );
+        $licenseText = "This dataset is licenced under the Creative Commons Attribution 4.0 International (CC BY 4.0). See https://creativecommons.org/licenses/by/4.0/";
+        $useLimitation->appendChild($doc->createElement("gco:CharacterString", $licenseText));
         $legalConstraints->appendChild($useLimitation);
 
         // accessConstraints = license
         $accessConstraints = $doc->createElement("gmd:accessConstraints");
         $accessCode = $doc->createElement("gmd:MD_RestrictionCode", "license");
-        $accessCode->setAttribute(
-            "codeList",
-            "http://www.isotc211.org/2005/resources/codeList.xml#MD_RestrictionCode"
-        );
+        $accessCode->setAttribute("codeList","http://www.isotc211.org/2005/resources/codeList.xml#MD_RestrictionCode");
         $accessCode->setAttribute("codeListValue", "license");
         $accessConstraints->appendChild($accessCode);
         $legalConstraints->appendChild($accessConstraints);
@@ -258,10 +196,7 @@ class DEIMSIso19139Controller extends ControllerBase
         // useConstraints = license
         $useConstraints = $doc->createElement("gmd:useConstraints");
         $useCode = $doc->createElement("gmd:MD_RestrictionCode", "license");
-        $useCode->setAttribute(
-            "codeList",
-            "http://www.isotc211.org/2005/resources/codeList.xml#MD_RestrictionCode"
-        );
+        $useCode->setAttribute("codeList","http://www.isotc211.org/2005/resources/codeList.xml#MD_RestrictionCode");
         $useCode->setAttribute("codeListValue", "license");
         $useConstraints->appendChild($useCode);
         $legalConstraints->appendChild($useConstraints);
@@ -272,10 +207,7 @@ class DEIMSIso19139Controller extends ControllerBase
         // language
         $lang = $doc->createElement("gmd:language");
         $langCode = $doc->createElement("gmd:LanguageCode", "eng");
-        $langCode->setAttribute(
-            "codeList",
-            "http://www.loc.gov/standards/iso639-2/"
-        );
+        $langCode->setAttribute("codeList","http://www.loc.gov/standards/iso639-2/");
         $langCode->setAttribute("codeListValue", "eng");
         $lang->appendChild($langCode);
         $dataId->appendChild($lang);
@@ -288,16 +220,12 @@ class DEIMSIso19139Controller extends ControllerBase
             $image = $record_information["attributes"]["general"]["images"][0];
             // File name (URL or file path)
             $fileName = $doc->createElement("gmd:fileName");
-            $fileName->appendChild(
-                $doc->createElement("gco:CharacterString", $image["url"])
-            );
+            $fileName->appendChild($doc->createElement("gco:CharacterString", $image["url"]));
             $browseGraphic->appendChild($fileName);
 
             // Optional description
             $fileDesc = $doc->createElement("gmd:fileDescription");
-            $fileDesc->appendChild(
-                $doc->createElement("gco:CharacterString", $image["alt"])
-            );
+            $fileDesc->appendChild($doc->createElement("gco:CharacterString", $image["alt"]));
             $browseGraphic->appendChild($fileDesc);
 
             // Nest and append
@@ -307,36 +235,26 @@ class DEIMSIso19139Controller extends ControllerBase
 
         // geographic extent
         if ($record_information["attributes"]["geographic"]["boundaries"]) {
-            $bbox_geom = $this->getBoundingBoxFromWKT(
-                $record_information["attributes"]["geographic"]["boundaries"]
-            );
+            $bbox_geom = $this->getBoundingBoxFromWKT($record_information["attributes"]["geographic"]["boundaries"]);
             $extent = $doc->createElement("gmd:extent");
             $exExtent = $doc->createElement("gmd:EX_Extent");
             $geo = $doc->createElement("gmd:geographicElement");
             $bbox = $doc->createElement("gmd:EX_GeographicBoundingBox");
 
             $west = $doc->createElement("gmd:westBoundLongitude");
-            $west->appendChild(
-                $doc->createElement("gco:Decimal", $bbox_geom["west"])
-            );
+            $west->appendChild($doc->createElement("gco:Decimal", $bbox_geom["west"]));
             $bbox->appendChild($west);
 
             $east = $doc->createElement("gmd:eastBoundLongitude");
-            $east->appendChild(
-                $doc->createElement("gco:Decimal", $bbox_geom["east"])
-            );
+            $east->appendChild($doc->createElement("gco:Decimal", $bbox_geom["east"]));
             $bbox->appendChild($east);
 
             $south = $doc->createElement("gmd:southBoundLatitude");
-            $south->appendChild(
-                $doc->createElement("gco:Decimal", $bbox_geom["south"])
-            );
+            $south->appendChild($doc->createElement("gco:Decimal", $bbox_geom["south"]));
             $bbox->appendChild($south);
 
             $north = $doc->createElement("gmd:northBoundLatitude");
-            $north->appendChild(
-                $doc->createElement("gco:Decimal", $bbox_geom["north"])
-            );
+            $north->appendChild($doc->createElement("gco:Decimal", $bbox_geom["north"]));
             $bbox->appendChild($north);
 
             $geo->appendChild($bbox);
@@ -345,15 +263,11 @@ class DEIMSIso19139Controller extends ControllerBase
             $dataId->appendChild($extent);
             $ident->appendChild($dataId);
             $root->appendChild($ident);
-        } else {
+        } 
+		else {
             // Extract coordinates from WKT string (assumed to be valid "POINT(lon lat)")
-            $wkt =
-                $record_information["attributes"]["geographic"]["coordinates"];
-            preg_match(
-                "/POINT\s*\(\s*([\d\.\-]+)\s+([\d\.\-]+)\s*\)/i",
-                $wkt,
-                $matches
-            );
+            $wkt = $record_information["attributes"]["geographic"]["coordinates"];
+            preg_match("/POINT\s*\(\s*([\d\.\-]+)\s+([\d\.\-]+)\s*\)/i", $wkt, $matches);
             $lon = $matches[1];
             $lat = $matches[2];
 
@@ -364,25 +278,12 @@ class DEIMSIso19139Controller extends ControllerBase
             $boundingPolygon = $doc->createElement("gmd:EX_BoundingPolygon");
 
             $polygon = $doc->createElement("gmd:polygon");
-            $gmlPoint = $doc->createElementNS(
-                "http://www.opengis.net/gml",
-                "gml:Point"
-            );
-            $gmlPoint->setAttribute(
-                "gml:id",
-                "centroid_or_representative_coordinates_of_site"
-            );
-            $gmlPoint->setAttribute(
-                "srsName",
-                "http://www.opengis.net/def/crs/EPSG/0/4326"
-            );
+            $gmlPoint = $doc->createElementNS("http://www.opengis.net/gml","gml:Point");
+            $gmlPoint->setAttribute("gml:id","centroid_or_representative_coordinates_of_site");
+            $gmlPoint->setAttribute("srsName","http://www.opengis.net/def/crs/EPSG/0/4326");
 
             // Add the coordinates (lat lon)
-            $pos = $doc->createElementNS(
-                "http://www.opengis.net/gml",
-                "gml:pos",
-                "$lat $lon"
-            );
+            $pos = $doc->createElementNS("http://www.opengis.net/gml","gml:pos","$lat $lon");
             $gmlPoint->appendChild($pos);
 
             // Nest everything into the correct hierarchy
@@ -391,7 +292,6 @@ class DEIMSIso19139Controller extends ControllerBase
             $geoElement->appendChild($boundingPolygon);
             $exExtent->appendChild($geoElement);
             $extent->appendChild($exExtent);
-
             $dataId->appendChild($extent);
             $ident->appendChild($dataId);
             $root->appendChild($ident);
@@ -403,18 +303,15 @@ class DEIMSIso19139Controller extends ControllerBase
         return $response;
     }
 
-    public function getBoundingBoxFromWKT(string $wkt): ?array
-    {
-        $wkt = trim($wkt);
-
+    public function getBoundingBoxFromWKT(string $wkt): ?array {
+        
+		$wkt = trim($wkt);
         $minLon = $maxLon = $minLat = $maxLat = null;
 
         // Match all coordinate groups inside parentheses (supports multiple polygons)
         preg_match_all("/\(\s*\(([^()]+)\)\s*\)/", $wkt, $matches);
 
-        if (empty($matches[1])) {
-            return null; // Invalid or unsupported WKT
-        }
+        if (empty($matches[1])) { return null; // Invalid or unsupported WKT }
 
         foreach ($matches[1] as $coords_str) {
             $points = preg_split("/,\s*/", trim($coords_str));
